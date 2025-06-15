f15145bb38086a5befd176e14787125fb4f2cd2b
melivasr <131542698+melivasr@users.noreply.github.com>
2025-06-15 15:05:04 -0600

    Terminado

--- Bloque 4/pregunta_2.m
+++ Bloque 4/pregunta_2.m
@@ -5,21 +5,16 @@
 function pregunta2()
   clc; clear;
   pkg load symbolic
-
-  % Definir simbólicamente la función f(x)
+% Definir simbólicamente la función f(x)
   syms x
   f = exp(x)*(26 - 10*x + x^2);  % Función dada
-
-  % Intervalo de integración
+% Intervalo de integración
   a = 5;
   b = 5.5;
-
-  % Tolerancia
+% Tolerancia
   tol = 1e-8;
-
-  % Calcular el valor mínimo de n
+% Calcular el valor mínimo de n
   n = cota_simpson_puntos(f, a, b, tol);
-
   fprintf('--- Método de Simpson Compuesto ---\n');
   fprintf('Valor de n: %d\n', n);
 endfunction
@@ -29,23 +24,23 @@ endfunction
 % método compuesto de Simpson sea menor que la tolerancia dada
 function n = cota_simpson_puntos(f, a, b, tol)
   syms x
-  % Cuarta derivada simbólica
+% Cuarta derivada simbólica
   f4 = diff(f, x, 4);
-  f4_abs = abs(f4);
+  f4_abs =abs(f4);
   f4_func = matlabFunction(f4_abs); % Convertir a función evaluable
 
-  % Encontrar máximo valor absoluto en [a, b]
-  [xmax, ~] = fminbnd(@(x) -f4_func(x), a, b); % Negamos "-f4_func(x)" para encontrar el máximo con fminbnd
+% Encontrar máximo valor absoluto en [a, b]
+  xmax = fminbnd(@(x) -f4_func(x), a, b); % Negamos "-f4_func(x)" para encontrar el máximo con fminbnd
   alpha_max = abs(f4_func(xmax));
 
-  % Buscar el menor n par que cumpla la condición
+% Buscar el menor n par que cumpla la condición
   n = 2;
   h = (b - a) / n;
   cota = ((b - a) * h^4 / 180) * alpha_max;
 
   while cota >= tol
     n += 2;
-    h = (b - a) / n;
+    h =(b - a) / n;
     cota = ((b - a) * h^4 / 180) * alpha_max;
   endwhile
 endfunction
